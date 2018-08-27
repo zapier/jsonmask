7b13d560effb48b8c4256b0eb714c6ace5d7759d
Craig Labenz <craig.labenz@gmail.com>
2018-08-27 11:56:11 -0400

    isort __init__

--- jsonmask/__init__.py
+++ jsonmask/__init__.py
@@ -1,7 +1,8 @@
-__project__ = 'jsonmask'
-__version__ = '0.0.0'
-
-VERSION = "{0} v{1}".format(__project__, __version__)
-
 from .mask import apply_json_mask, should_include_variable
 from .parsing import parse_fields
+
+
+__project__ = 'jsonmask'
+__version__ = '0.1.0'
+
+VERSION = "{0} v{1}".format(__project__, __version__)
